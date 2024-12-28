76aaa447ecf48dd8a38b34850daa09182f1d8eb8
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-12-28 15:52:21 +0530

    Add project files.

---
+++ StudentRegistration/Controllers/StudentRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentRegistration.Controllers
+{
+    public class StudentRegistration : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+         public IActionResult Create()
+        {
+            return View();
+        }
+         public IActionResult Edit()
+        {
+            return View();
+        }
+
+    }
+
+}
